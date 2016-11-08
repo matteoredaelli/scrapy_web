f8f6a3c6e21c2ef64524937e71df1ce0f43c9435
matteo <matteo@dell.homenet.telecomitalia.it>
2016-11-08 22:14:00 +0100

    license header and cal prefissi comuni italiani

--- go.sh
+++ go.sh
@@ -2,4 +2,6 @@ for file in data/corriere.it/dizionario_italiano.json : do
   mv $file $file.old
 done
 
+wget http://www.istat.it/it/files/2011/01/Elenco-codici-statistici-e-denominazioni-al-01_07_2016.csv
+
 scrapy crawl dizionario_italiano_corriere -t jsonlines -o data/corriere.it/dizionario_italiano.json
---
+++ scrapy_web/spiders/cap_comuni_italiani_it.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+#   scrapy_web
+#    Copyright (C) 2016-2017 Matteo.Redaelli@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# usage:
+#   scrapy crawl cap-comuni-italiani -t jsonlines -o data/comuni-italiani.it/cap-comuni-italiani.json
+
+import scrapy
+import re
+
+class CapComuniItalianiSpider(scrapy.Spider):
+    name = "cap-comuni-italiani"
+    allowed_domains = ["comuni-italiani.it"]
+    start_urls = (
+        'http://www.comuni-italiani.it/alfa/001.html',
+    )
+
+    def parse(self, response):
+        for url in response.xpath('//table[@border="1"]//a/@href').extract():
+            if bool(re.search("^\d\d\d\.html", url)):
+                absolute_url = response.urljoin(url)
+                yield scrapy.Request(absolute_url, callback=self.parse)
+            elif bool(re.search("index.html", url)):
+                absolute_url = response.urljoin(url)
+                yield scrapy.Request(absolute_url, callback=self.parse_city)
+    
+    def parse_city(self, response):
+        record = {}
+        for row in response.xpath('//tr'):
+            if row.xpath('td[@class="ivoce"]'):
+                field = row.xpath('td[@class="ivoce"]//text()').extract_first()
+                value = row.xpath('td[2]//text()').extract_first()
+                record[field] = value
+        yield record
+
