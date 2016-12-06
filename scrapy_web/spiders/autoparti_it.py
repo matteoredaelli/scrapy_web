08141bb8fc7b999ff7cf6ca0289027854e6eb9ec
matteo <matteo@dell.homenet.telecomitalia.it>
2016-12-06 21:52:49 +0100

    fix in picture_url, added prod_url and source

--- scrapy_web/spiders/autoparti_it.py
+++ scrapy_web/spiders/autoparti_it.py
@@ -18,7 +18,7 @@
 #
 
 import scrapy
-import re
+import datetime, re
 
 def clean_text(text):
     if text is None or text == "":
@@ -33,12 +33,14 @@ class AutopartiIt(scrapy.Spider):
     start_urls = ['http://www.autoparti.it/pneumatici/%d-pollici?page=1' % n for n in [14,15,16,17,18]]
 
     def parse(self, response):
+        ts = datetime.datetime.now()
         for entry in response.xpath('//div[@class="tires_listing"]/div[@class="item"]'):
             model = entry.xpath('//a[@class="prod_link"]/text()').extract_first()
             description = entry.xpath('.//span[@class="nam_model"]/text()').extract_first()
             ean = entry.xpath('.//div[@class="nr"]/span[1]/text()').extract_first()#.replace("EAN: ","")
             id  = entry.xpath('.//div[@class="nr"]/span[2]/text()').extract_first()#.replace("MPN: ","")
-            picture_url = entry.xpath('.//div[@class="item"]//img[@class="tires_item_image"]/@src').extract_first()
+            picture_url = entry.xpath('.//img[@class="tires_item_image"]/@src').extract_first()
+            prod_url = entry.xpath('.//a[@class="prod_link"]/@href').extract_first()
             price = entry.xpath('.//span[@class="new_pr"]/text()').extract_first()
             season = entry.xpath('.//div[contains(@class, "tires_season")]/@class').extract_first()#.replace("tires_season ","")
             yield {"id": id,
@@ -46,8 +48,11 @@ class AutopartiIt(scrapy.Spider):
                        "ean": ean,
                        "model": model,
                        "picture_url": picture_url,
+                       "prod_url": prod_url,
                        "price": price,
-                       "season": season}
+                       "season": season,
+                       "source": "autoparti.it",
+                       "ts": ts}
 
         next_page = response.xpath('//span[@class="next"]/a/@href').extract_first()
         if next_page != None:
